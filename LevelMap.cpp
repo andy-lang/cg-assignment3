0d942ab88a1b60bef91ffd080d1c45209d99acf1
Gavin <gmeredith15855@gmail.com>
2015-05-23 10:52:44 +0930

    G: still playing around with the map

--- LevelMap.cpp
+++ LevelMap.cpp
@@ -3,11 +3,8 @@
 std::vector<Object> generateLevelMap(int programID, std::vector<Object> objects){
 	std::vector<Object> obj = objects;
 
-	Object obj1(programID, "geom/wall_cube/wall_cube.obj", glm::vec3(0.0f, 0.f, 0.0f), glm::vec3(0.0f, 0.0f, 10.0f), 10.0f);
+	Object obj1(programID, "geom/wall_cube/wall_cube.obj", glm::vec3(0.0f, 0.f, 0.0f), glm::vec3(0.0f, 0.0f, 10.0f), 0.5f);
     obj.push_back(obj1);
-
-    //Object obj2(programID, "geom/cube-tex/cube-tex.obj", glm::vec3(0.0f, 0.f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), 1.0f);
-    //obj.push_back(obj2);
 
 	return obj;
 }
