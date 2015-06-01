3306357952adfb0e565f41474ce7fb576c043158
HandyAndy136 <langy26@gmail.com>
2015-06-01 18:21:05 +0930

    Added brightness modifier for Light source.

--- Light.hpp
+++ Light.hpp
@@ -30,8 +30,9 @@ public:
 	 * @param	ambient		The ambient light values for the light source.
 	 * @param	diffuse		The diffuse light values for the light source.
 	 * @param	specular	The specular light values for the light source.
+	 * @param	brightness	The brightness of the light source. Set to a value of 1.0f by default.
 	 */
-    Light(glm::vec3 position, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
+    Light(glm::vec3 position, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float brightness = 1.0f);
 
 	/* Light destructor. */
     ~Light();
@@ -42,12 +43,14 @@ public:
 	glm::vec3 getAmbient();
 	glm::vec3 getDiffuse();
 	glm::vec3 getSpecular();
+	float getBrightness();
 
 	/* Setters */
 	void setPosition(glm::vec3 position);
 	void setAmbient(glm::vec3 ambient);
 	void setDiffuse(glm::vec3 diffuse);
 	void setSpecular(glm::vec3 specular);
+	void setBrightness(float brightness);
 
 private:
 	/* Basic Light constructor. We make this private because there's no sense in calling it. */
@@ -57,6 +60,7 @@ private:
 	glm::vec3 mAmbient; // ambient light
 	glm::vec3 mDiffuse; // diffuse light
 	glm::vec3 mSpecular; // specular light
+	float mBrightness;
 };
 
 
--- pf-light.vert
+++ pf-light.vert
@@ -16,7 +16,7 @@ out vec2 texCoord;
 void main(void) {
     texCoord = vec2(a_tex_coord.x, 1.0f - a_tex_coord.y); // need to invert tex coords' y value, due to disparity between how OBJ files and OpenGL implement texture mapping
 
-	lightPosMatrix = model_matrix * view_matrix;
+	lightPosMatrix = view_matrix;
 	normal = normalize(normal_matrix * a_normal);
 	vertex = view_matrix * model_matrix * vec4(a_vertex, 1.0f);
     gl_Position = projection_matrix * vertex;
